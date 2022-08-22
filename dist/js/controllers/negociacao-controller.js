758d4ec3e581eb7805b5a6ccadf59debef1e5bdb
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2022-08-22 20:58:32 -0300

    alteração de arquivos .ts
    Código foi refatorado e foi adicionado arquivos em TypeScript.

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,19 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ","));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+}
