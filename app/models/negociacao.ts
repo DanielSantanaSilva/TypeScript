c432f0b7048e8718a73f48cb35134da4d6f91b0f
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2022-08-29 19:08:05 -0300

    update
    Nova maneira de declaração de array com generics
    O tipo ReadonlyArray
    O modificador readonly
    Getters vs propriedades públicas em modo de leitura
    Programação defensiva

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,16 @@
 export class Negociacao {
-  private _data: Date;
-  private _quantidade: number;
-  private _valor: number;
+  constructor(
+    private _data: Date,
+    public readonly quantidade: number,
+    public readonly valor: number
+  ) {}
 
-  constructor(data: Date, quantidade: number, valor: number) {
-    this._data = data;
-    this._quantidade = quantidade;
-    this._valor = valor;
+  get volume(): number {
+    return this.quantidade * this.valor;
   }
 
   get data(): Date {
-    return this._data;
-  }
-
-  get quantidade(): number {
-    return this._quantidade;
-  }
-
-  get valor(): number {
-    return this._valor;
-  }
-
-  get volume(): number {
-    return this._quantidade * this._valor;
+    const data = new Date(this.data.getTime());
+    return data;
   }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,19 +1,14 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
+    }
+    get data() {
+        const data = new Date(this.data.getTime());
+        return data;
     }
 }
